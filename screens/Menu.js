64943632119eecba2195b1da84ce51a098660091
joão victor schamne <schamnejv@hotmail.com>
2023-05-26 13:01:11 -0300

    novo branch joao

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,63 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import RestaurantCard from '../components/RestaurantCard';
+import BottomTabNav from '../components/BottomTabNav';
 
 export default function Menu() {
 
-  const navigation = useNavigation();
-
   const restaurantes = useState(["res1", "res2", "res3"])
 
-  return (
+  return(
     <View style={styles.container}>
-    
-      <Text
-        style={{marginBottom: 35, fontSize: 30}}>Menu</Text>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 30, marginBottom: 50}}>Restaurantes</Text>
+        <RestaurantCard name={'Churassic Park'}/>
+        <RestaurantCard name={'Douglas Lanches'}/>
+        <RestaurantCard name={'Marmitex do Creitons'}/>
+        <RestaurantCard name={'Shinobi Lamen'}/>
+        <BottomTabNav></BottomTabNav>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fcc40d',
+    justifyContent: 'flex-start',
+    backgroundColor: '#f9f1f7',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 32,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#000',
-    backgroundColor: '#fff',
-    borderRadius: 4,
-    padding: 8,
-    marginTop: 15,
-    margin: 8,
-    width: '80%',
-  },
-  button: {
-    backgroundColor: '#008080',
-    padding: 15,
-    width: 200,
-    justifyContent: 'center',
-    alignItems:'center',
-    borderRadius: 4,
-    marginTop: 32,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-});
+})
